b1dad88bed7f51cdeb33f40cbd21c795b288cce7
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-24 17:33:12 +0200

    fixed __init__.py at several levels

---
+++ main.py
@@ -0,0 +1,4 @@
+import gym
+import rex_gym
+
+env = gym.make("RexGallop-v0")
--- rex_gym/envs/__init__.py
+++ rex_gym/envs/__init__.py
@@ -1,1 +1,5 @@
-
+from gym.gallop_env import RexReactiveEnv
+from gym.poses_env import RexPosesEnv
+from gym.standup_env import RexStandupEnv
+from gym.turn_env import RexTurnEnv
+from gym.walk_env import RexWalkEnv
