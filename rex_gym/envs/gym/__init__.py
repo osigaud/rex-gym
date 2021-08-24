9b162de43b18d7abb1968c1dd701e7aad7e744d3
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-24 16:45:55 +0200

    added __init__.py in envs and gym

--- rex_gym/envs/gym/__init__.py
+++ rex_gym/envs/gym/__init__.py
@@ -1,1 +1,5 @@
-
+from envs.gym.gallop_env import RexGallopEnv
+from envs.gym.poses_env import RexPosesEnv
+from envs.gym.standup_env import RexStandupEnv
+from envs.gym.turn_env import RexTurnEnv
+from envs.gym.walk_env import RexWalkEnv
